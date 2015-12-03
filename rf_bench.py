9b650d7e4b5ed08857c36268e57f7688d91f11ae
ZENGXH <xzengaf@ust.hk>
2015-12-03 19:09:39 +0100

    12/3 add config

---
+++ rf_bench.py
@@ -0,0 +1,47 @@
+# randomforest_bench.py
+import logging
+import my_io
+import classification_baseline
+from sklearn import svm
+import numpy as np
+#from sklearn.metrics import classification
+from numpy import linalg as LA
+
+my_io.setUp('./digit/')
+
+my_io.startLog(__name__)
+logger = logging.getLogger(__name__)
+
+"""y,X,trainData,testData = my_io.readCsv()
+portion = 0.2
+seed = 1
+X_test, X_train, y_train, y_test = classification_baseline.splitData(X,y,portion,seed)
+
+logger.info('init svm classifier')
+svc = svm.SVC(probability = True)
+logger.info('fitting svc')
+svc.fit(X_train, y_train)
+logger.info('start predict')
+predict_probs = svc.predict_proba(X_test)
+
+predict = my_io.toZeroOne(predict_probs)
+# error = classification.zero_one_loss(y_test, predict)
+loss = np.subtract(predict,y_test)
+
+error = LA.norm(loss)
+logger.info('zero one loss %f',error)
+"""
+
+# for submission
+y,X,trainData,testData = my_io.readCsv()
+logger.info('init svm classifier')
+"""
+svc = svm.SVC(probability = True)
+logger.info('fitting svc')
+svc.fit(X, y)
+logger.info('start predict')
+predict_probs = svc.predict_proba(testData)
+
+predict = my_io.toZeroOne(predict_probs)
+"""
+my_io.writeCsv(predict)
